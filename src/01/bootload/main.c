d704e2679482f2173601c1e3dd8563bd30e12e9f
bunkyu3 <40335967+bunkyu3@users.noreply.github.com>
2018-07-21 23:52:17 +0900

    3rd step
    enable to write static variable and initialize it

--- src/01/bootload/main.c
+++ src/01/bootload/main.c
@@ -2,15 +2,53 @@
 #include "serial.h"
 #include "lib.h"
 
+static int init(void)
+{
+	/*
+	以下はリンカ・スクリプトで定義してあるシンボル
+	リンカ・スクリプトで定義されたシンボルを参照可能にする
+	*/
+	extern int erodata, data_start, edata, bss_start, ebss;	
+
+	/*
+	データ領域とBSS領域を初期化する。この処理以降でないと、
+	グローバル変数が初期化されていないので注意
+	*/
+	memcpy(&data_start, &erodata, (long)&edata - (long)&data_start);
+	memset(&bss_start, 0, (long)&ebss - (long)&bss_start);
+
+	/*シリアルの初期化*/
+	serial_init(SERIAL_DEFAULT_DEVICE);		//シリアルデバイスを初期化
+	
+	return 0;
+}
+
+int global_data = 0x10;
+int global_bss;
+static int static_data = 0x20;
+static int static_bss;
+
+static void printval(void)
+{
+	puts("glocal_data	= "); putxval(global_data, 0); puts("\n");
+	puts("glocal_bss	= "); putxval(global_bss,	 0); puts("\n");
+	puts("static_data	= "); putxval(static_data, 0); puts("\n");
+	puts("static_bss	= "); putxval(static_bss,	 0); puts("\n");
+}	
+
 int main(void)
 {
-  serial_init(SERIAL_DEFAULT_DEVICE);	//シリアルデバイスを初期化
- 
+	init();  
   puts("Hello World!\n");		//"Hello World!"を出力
 
-	putxval(0x10, 0);		puts("\n");
-	putxval(0xffff, 0);	puts("\n");
- 
+	printval();
+	puts("overwrite variable.\n");
+	global_data = 0x20;
+	global_bss 	= 0x30;
+	static_data = 0x40;
+	static_bss 	= 0x50;
+	printval();	
+
   while (1)				//無限ループで停止
     ;
 
--- src/01/bootload/startup.s
+++ src/01/bootload/startup.s
@@ -3,7 +3,7 @@
 	.global	_start
 #	.type	_start,@function
 _start:				#ラベル「_start」の定義
-	moV.l	#0xffff00,sp	#スタックポインタの設定
+	mov.l	#_stack,sp	#スタックポインタの設定
 	jsr	@_main		#main()の呼び出し
 
 1:
